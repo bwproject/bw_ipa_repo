d2e604e9856966bb66210720ccd0bf4481970909
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-12-03 13:48:06 +0500

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,9 +1,9 @@
-import uvicorn
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pathlib import Path
-from dotenv import load_dotenv
-import os
+import uvicorn
 
 load_dotenv()
 
@@ -13,13 +13,16 @@ BASE_PATH = Path("repo")
 PACKAGES = BASE_PATH / "packages"
 IMAGES = BASE_PATH / "images"
 
+
 @app.get("/repo/index.json")
 async def get_index():
     return FileResponse(BASE_PATH / "index.json")
 
+
 @app.get("/repo/packages/{file_name}")
 async def get_ipa(file_name: str):
     return FileResponse(PACKAGES / file_name)
+
 
 @app.get("/repo/images/{file_name}")
 async def get_image(file_name: str):
@@ -27,6 +30,16 @@ async def get_image(file_name: str):
 
 
 async def start_server():
-    host = os.getenv("SERVER_HOST", "0.0.0.0")
-    port = int(os.getenv("SERVER_PORT", "8000"))
-    uvicorn.run(app, host=host, port=port)
+    """
+    Асинхронный запуск uvicorn внутри общего event loop
+    """
+    config = uvicorn.Config(
+        app,
+        host=os.getenv("SERVER_HOST", "0.0.0.0"),
+        port=int(os.getenv("SERVER_PORT", "8000")),
+        log_level="info",
+        loop="asyncio",
+    )
+
+    server = uvicorn.Server(config)
+    await server.serve()
