9e6c99380fc9739dab73df21ba0b7d0ca71a09ee
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-12-03 14:17:29 +0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -7,9 +7,5 @@ load_dotenv()
 
 async def start_bot():
     app = Application.builder().token(os.getenv("BOT_TOKEN")).build()
-
-    # регистрация всех хендлеров
     register_handlers(app)
-
-    # правильный запуск polling в python-telegram-bot v20+
     await app.run_polling()
