22eb1de6d5ad5b6c3ae664497b81dfa068713232
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-12-03 11:52:21 +0500

    Implement basic bot setup with Telegram API

---
+++ bot/bot.py
@@ -0,0 +1,13 @@
+from telegram.ext import Application
+import os
+from dotenv import load_dotenv
+from bot.handlers import register_handlers
+
+load_dotenv()
+
+async def start_bot():
+    app = Application.builder().token(os.getenv("BOT_TOKEN")).build()
+    register_handlers(app)
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
